f616df2bc31e0708bd7ec20385c9086d009a02ef
FadiAmori <fadiamorri2002@gmail.com>
2025-01-27 15:32:26 +0100

    Initial commit for minimal Maven project

---
+++ src/test/java/tn/esprit/test2/Test2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
